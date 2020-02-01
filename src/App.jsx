c4699ca760e082642906ce2ff726c7f17b2e14bd
Alex Glau <alexglau@alizglau.planet-a.ru>
2020-02-01 12:11:35 +0500

    App.js => App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from './hoc/Layout/Layout.jsx';
+
+function App() {
+  return (
+    <Layout>
+      <div className="" style={{width: 400, border: '1px solid black'}}>
+        <h1>Layout is working</h1>
+      </div>
+    </Layout>
+  );
+}
+
+export default App;
