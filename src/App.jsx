21ec64496becd783ce698cfa31933d1997934b4b
Alex Glau <alexglau@alizglau.planet-a.ru>
2020-02-01 18:05:17 +0500

    Main page has been plugged in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Layout from './hoc/Layout/Layout.jsx';
 
+import Quiz from './containers/Quiz/Quiz.jsx';
+
+
 function App() {
   return (
     <Layout>
-      <div className="" style={{width: 400, border: '1px solid black'}}>
-        <h1>Layout is working</h1>
-      </div>
+      <Quiz />
     </Layout>
   );
 }
