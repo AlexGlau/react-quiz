ddf59dfe6af2ce702c29bc074554b574e0027cbb
Alex Glau <alexglau@alizglau.planet-a.ru>
2020-02-02 11:38:46 +0500

    Click handler

--- src/containers/Quiz/Quiz.jsx
+++ src/containers/Quiz/Quiz.jsx
@@ -7,14 +7,20 @@ class Quiz extends Component {
   state = {
     quiz: [
       {
+        question: 'What color is the sky?',
+        rightAnswer: 2,
         answers: [
-          {text: 'Question 1'},
-          {text: 'Question 2'},
-          {text: 'Question 3'},
-          {text: 'Question 4'}
+          {text: 'Question 1', id: 1},
+          {text: 'Question 2', id: 2},
+          {text: 'Question 3', id: 3},
+          {text: 'Question 4', id: 4}
         ]
       }
     ]
+  }
+
+  onAnswerClickHandler = (answerId) => {
+    console.log(`answer ID: ${answerId}`)
   }
 
   render() {
@@ -22,7 +28,11 @@ class Quiz extends Component {
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
           <h1>Answer the all questions</h1>
-          <ActiveQuiz answers={this.state.quiz[0].answers} />
+          <ActiveQuiz
+            question={this.state.quiz[0].question}
+            answers={this.state.quiz[0].answers}
+            onAnswerClick={this.onAnswerClickHandler}
+          />
         </div>
       </div>
     );
