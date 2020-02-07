566abceb4faf7fa766044d6bbd0f447551fcf53b
ALizGlau <glaualex89@gmail.com>
2020-02-07 18:53:04 +0500

    FinishedQuiz component

---
+++ src/components/FinishedQuiz/FinishedQuiz.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import classes from './FinishedQuiz.module.css';
+
+
+const FinishedQuiz = (props) => {
+  return (
+    <div className={classes.FinishedQuiz}>
+      <ul>
+        <li>
+          <strong>1. </strong>
+          How are you
+          <i className={'fa fa-times ' + classes.error}></i>
+        </li>
+        <li>
+          <strong>2. </strong>
+          How are you
+          <i className={'fa fa-check ' + classes.success}></i>
+        </li>
+      </ul>
+
+      <p>Correct answers: 4 from 10</p>
+
+      <div>
+        <button>Repeat</button>
+      </div>
+    </div>
+  );
+}
+
+export default FinishedQuiz;
