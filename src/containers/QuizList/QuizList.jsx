80244cc245e9d1a735a2668d10e129d020424878
ALizGlau <glaualex89@gmail.com>
2020-02-10 13:13:08 +0500

    Quiz list pages

--- src/containers/QuizList/QuizList.jsx
+++ src/containers/QuizList/QuizList.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import classes from './QuizList.module.css';
+
 
 class QuizList extends Component {
+  renderQuizes() {
+    return [1, 2, 3].map((quiz, i) => {
+      return (
+        <li key={i}>
+          <NavLink
+            to={'quiz/' + quiz}
+          >
+            Test {quiz}
+          </NavLink>
+        </li>
+      )
+    })
+  };
+
   render() {
     return (
-      <div>
-        <h1>Quiz List</h1>
+      <div className={classes.QuizList}>
+        <div>
+          <h1>Quiz List</h1>
+
+          <ul>
+            {this.renderQuizes()}
+          </ul>
+        </div>
       </div>
     );
   }
