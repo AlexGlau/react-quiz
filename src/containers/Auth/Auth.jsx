8b6b748951730834295a3cc19fda71accac8ccf7
ALizGlau <glaualex89@gmail.com>
2020-02-23 10:38:00 +0500

    Form validation

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -6,6 +6,7 @@ import Input from '../../components/UI/Input/Input.jsx';
 class Auth extends Component {
 
   state = {
+    isFormValid: false,
     formControls: {
       email: {
         value: '',
@@ -78,8 +79,15 @@ class Auth extends Component {
 
     formControls[controlName] = control;
 
+    let isFormValid = true;
+
+    Object.keys(formControls).forEach(name => {
+      isFormValid = formControls[name].valid && isFormValid
+    });
+
     this.setState({
-      formControls
+      formControls,
+      isFormValid
     })
   };
 
@@ -117,12 +125,14 @@ class Auth extends Component {
               <Button
                 type='success'
                 onClick={this.loginHandler}
+                disabled={!this.state.isFormValid}
               >
                 Sign in
               </Button>
               <Button
                 type='primary'
                 onClick={this.signUpHandler}
+                disabled={!this.state.isFormValid}
               >
                 Sign up
               </Button>
