cf6eb9c1da3e76104be041ef3efc2241927808e9
alizglau <glaualex89@gmail.com>
2020-04-05 12:58:34 +0500

    Changing routes depending on auth status

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout.jsx';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
 
 import Quiz from './containers/Quiz/Quiz.jsx';
 import Auth from './containers/Auth/Auth.jsx';
 import QuizCreator from './containers/QuizCreator/QuizCreator.jsx';
 import QuizList from './containers/QuizList/QuizList.jsx';
+import { connect } from 'react-redux';
+import Logout from './components/Logout/Logout';
 
 
-function App() {
-  return (
-    <Layout>
+class App extends Component {
+  render() {
+
+    let routes = (
       <Switch>
         <Route path='/auth' component={Auth} />
-        <Route path='/quiz-creator' component={QuizCreator} />
         <Route path='/quiz/:id' component={Quiz} />
         <Route path='/' component={QuizList} />
+        <Redirect to="/" />
       </Switch>
-    </Layout>
-  );
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path='/quiz-creator' component={QuizCreator} />
+          <Route path='/quiz/:id' component={Quiz} />
+          <Route path='/' component={QuizList} />
+          <Route path='/logout' component={Logout} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+
+    return (
+      <Layout>
+        { routes }
+      </Layout>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: !!state.auth.token
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(App));
--- src/components/Navigation/Drawer/Drawer.jsx
+++ src/components/Navigation/Drawer/Drawer.jsx
@@ -3,19 +3,13 @@ import { NavLink } from 'react-router-dom';
 import classes from './Drawer.module.css';
 import Backdrop from '../../UI/Backdrop/Backdrop.jsx';
 
-const links = [
-  {to: '/', label: 'List', exact: true},
-  {to: '/auth', label: 'Authorization', exact: false},
-  {to: '/quiz-creator', label: 'Create a test', exact: false},
-];
-
 
 class Drawer extends Component {
   clickHandler = () => {
     this.props.onClose();
   };
 
-  renderLinks() {
+  renderLinks(links) {
     return links.map((link, i) => {
       return (
         <li key={i}>
@@ -39,11 +33,25 @@ class Drawer extends Component {
       cls.push(classes.close);
     }
 
+    const links = [
+      {to: '/', label: 'List', exact: true}
+    ];
+
+    console.log('auth: ', this.props.isAuthenticated);
+
+
+    if (this.props.isAuthenticated) {
+      links.push({to: '/quiz-creator', label: 'Create a test', exact: false});
+      links.push({to: '/logout', label: 'Logout', exact: false});
+    } else {
+      links.push({to: '/auth', label: 'Authorization', exact: false});
+    }
+
     return (
       <React.Fragment>
         <nav className={cls.join(' ')}>
           <ul>
-            {this.renderLinks()}
+            {this.renderLinks(links)}
           </ul>
         </nav>
         {this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null}
